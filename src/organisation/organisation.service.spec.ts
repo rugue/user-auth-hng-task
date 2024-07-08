9a06a5e92587e93d116f3165fb66f6c8d5fc172a
Osarugue Enehizena <nehirugue@gmail.com>
2024-07-08 22:17:53 +0100

    Updated unit test

--- src/organisation/organisation.service.spec.ts
+++ src/organisation/organisation.service.spec.ts
@@ -41,7 +41,7 @@ describe('OrganisationService', () => {
 
   it(`Users should only see data from all organisations they belong to`, () => {
     organisationService.getUserOrganisations(mockUser).then((result) => {
-      expect(result).toEqual([mockOrganisation]);
+      expect(result).toEqual({ organisations: [mockOrganisation] });
 
       // Ensure that the find method was called with the correct parameters
       expect(mockOrganisationRepository.find).toHaveBeenCalledWith({
